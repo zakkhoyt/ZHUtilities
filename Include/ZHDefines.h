358f19ee114fae8920dc073b6fd5df6fb37c7104
Zakk Hoyt <vaporwarewolf@gmail.com>
2016-04-08 18:34:02 -0700

    Updated macros

--- Include/ZHDefines.h
+++ Include/ZHDefines.h
@@ -16,7 +16,7 @@
 //******************************************************************************
 // Trace defines ***********************************************************
 #define ZH_LOG(...) NSLog(@"%@", [NSString stringWithFormat:__VA_ARGS__]);
-#define ZH_LOG_TRACE  NSLog(@"%s:%d ***** TRACE", __FUNCTION__, __LINE__);
+#define ZH_LOG_TRACE NSLog(@"%s:%d ***** TRACE", __FUNCTION__, __LINE__);
 #define ZH_LOG_INFO(...) NSLog(@"%s:%d ***** INFO: %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]);
 #define ZH_LOG_WARNING(...) NSLog(@"%s:%d ***** WARNING: %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]);
 #define ZH_LOG_ERROR(...) NSLog(@"%s:%d ***** ERROR: %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]);
@@ -25,7 +25,9 @@
 #define ZH_LOG_TEST(...) NSLog(@"%s:%d\n********************************************************* TESTING: %@ *********************************************************", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]);
 
 #if defined(DEBUG)
-#define ZH_LOG_CRITICAL(...) NSLog(@"%s:%d ***** CRITICAL ERROR: %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]); assert(0);
+#define ZH_LOG_CRITICAL(...)                                                                                   \
+    NSLog(@"%s:%d ***** CRITICAL ERROR: %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]); \
+    assert(0);
 #define ZH_LOG_DEBUG(...) NSLog(@"%s:%d ***** DEBUG: %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]);
 #define ZH_LOG_VERBOSE(...) NSLog(@"%s:%d ***** VERBOSE: %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:__VA_ARGS__]);
 #else
@@ -38,25 +40,12 @@
 
 //******************************************************************************
 // Device/OS defines ***********************************************************
-#define ZH_IS_WIDESCREEN ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
-#define ZH_IS_IPHONE ( [ [ [ UIDevice currentDevice ] model ] isEqualToString: @"iPhone" ] )
-#define ZH_IS_IPOD   ( [ [ [ UIDevice currentDevice ] model ] isEqualToString: @"iPod touch" ] )
-#define ZH_IS_IPHONE_5 ( IS_IPHONE && IS_WIDESCREEN )
-#define ZH_SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define ZH_IS_WIDESCREEN (fabs((double)[[UIScreen mainScreen] bounds].size.height - (double)568) < DBL_EPSILON)
+#define ZH_IS_IPHONE ([[[UIDevice currentDevice] model] isEqualToString:@"iPhone"])
+#define ZH_IS_IPOD ([[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
+#define ZH_IS_IPHONE_5 (IS_IPHONE && IS_WIDESCREEN)
+#define ZH_SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 // end device/OS defines ***************************************************************
 //******************************************************************************
-
-
-//******************************************************************************
-// NOTES AND OTHER CONFIGURATIONS **********************************************
-// Image sizes are defined here:
-
-// Items below here are number specific and generally will not be changed ******
-#define SM_ANIMATION_DURATION               0.3
-
-// End notes and configurations ************************************************
-//******************************************************************************
-
-
 
 #endif /* ZHDefines_h */
